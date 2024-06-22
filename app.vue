f21c8441ad8ad390b2883733b68a7da19748dde9
TangoCharlie <elouanreymond@tutanota.com>
2024-06-22 18:07:14 +0200

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="custom">
+      <template #header>
+        <p>Custom header content</p>
+      </template>
+      <NuxtPage />
+</NuxtLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+</script>
+
+<template>
+  <div>
+    <h1>Elouan Reymond</h1>
+  </div>
+</template>
